d1ab1ea300a20dfb2a25d4f2eff9e3b6521ae15a
Dmitrii Trebin <d.trebin@rambler.ru>
2022-03-18 18:33:56 +0300

    Замена типов данных

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         double costTicket = 1125.23;
-        int points = 20;
+        byte points = 20;
         int mili = (int) costTicket / points;
 
         System.out.println("Вам начислено " + mili + " миль.");
